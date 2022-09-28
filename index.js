f467eb71e23552067243bb718b495ea1be5c22d1
golam mostafa <gmnayeem25@gmail.com>
2022-09-28 18:58:59 +0600

    Calculator works successfully

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+let resultField = $("#result")
+
+function insertNumber(number) {
+    let existingNumber = resultField.val()
+    resultField.val(existingNumber + number)
+}
+
+function clearResult() {
+    resultField.val('')
+}
+
+function calculate() {
+    let result = eval(resultField.val())
+    resultField.val(result)
+}
+
+function removeNumber() {
+    let presentNumber = resultField.val()
+    if(presentNumber != '') {
+        resultField.val( presentNumber.slice(0, -1) )
+    }
+    
+}
